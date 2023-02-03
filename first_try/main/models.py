070b5ec513b6805047bc28809ddf312db57a590a
Рыжий <122425980+Detepl@users.noreply.github.com>
2023-02-03 17:07:21 +0300

    add db table and register admin

--- first_try/main/models.py
+++ first_try/main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField("login",max_length=25)
+    pasw = models.CharField("password",max_length=25)
+
+    def __str__(self):
+        return self.name
+        return self.pasw
+
+       
